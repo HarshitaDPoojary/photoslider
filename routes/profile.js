fdab0ca853eae9e7c9054b06ab6cfe91cf34af83
monalid1326 <dhakemonali98@gmail.com>
2019-08-01 13:14:16 +0530

    post update and updateimg

--- routes/profile.js
+++ routes/profile.js
@@ -141,8 +141,110 @@ router.get('/removeImg',verifyToken,async(req,res)=>{
             })  
         }
     });
+});
 
+router.post('/update',verifyToken ,upload.none(), (req,res)=>{
+    jwt.verify(req.token, 'HelloAlbumProject170619' ,(err, data)=>{
+        if(err) {
+            res.status(403).send("the token is invalid or expired");  
+        }
+        else{
+            Joi.validate({firstname:req.body.firstname,lastname: req.body.lastname},schema, async (err,value)=>{
+            if(err)
+            {
+                console.log("\n"+err);
+                res.send(err.details[0].message);
+            }
+            else{
 
+            
+                    console.log(req.body);
+                    User.updateOne({ emailid: data.user1.emailid }, { $set: { firstname: req.body.firstname, lastname:req.body.lastname } }).then(user => {
+                        let emailid=data.user1.emailid;
+                        jwtBlacklist.blacklist(req.token);
+                
+
+                    user1={
+                        firstname:req.body.firstname,
+                        lastname:req.body.lastname,
+                        emailid:emailid
+                    }
+            
+                    jwt.sign({user1},"HelloAlbumProject170619",  { expiresIn: '3000s' } ,(e, token) => {
+                    if(e)
+                    {
+                        res.status(403).send('Bad request');
+                    }
+                    res.json({"message":"profileUpdated","newtoken":token});
+                    console.log(token);
+            
+                    
+                });
+                
+            })
+           .catch(err=>{
+               res.send(err);
+           });
+        }});
+        }
+    });
+});
+
+router.post('/updateImg',verifyToken,upload.single('file'),async(req,res)=>{
+    jwt.verify(req.token, 'HelloAlbumProject170619' ,(err, data)=>{
+        if(err) {
+            res.status(403).send("the token is invalid or expired");  
+        }
+        else{
+
+            if(req.body.emailid== data.user1.emailid){
+                try{
+                    var newImg=fs.readFileSync(path.join(path.dirname(process.mainModule.filename),'uploads',data.user1.emailid+'.jpg'));
+                
+                    if(newImg){
+                        var encImg=newImg.toString('base64');
+                        contentType="image/png";
+                        img= Buffer(encImg, 'base64');
+                        User.updateOne({ emailid: data.user1.emailid }, { $set: { contentType: contentType, img:img } })
+                        .then(user => {
+                            res.json({messsage: "Profile pic updated"});
+                        })
+                        .catch(err=>{
+                            res.send(err);
+                        })
+    
+                        fs.unlink(path.join(path.dirname(process.mainModule.filename),'uploads',req.body.emailid+'.jpg')  , (err) => {
+                            if (err) throw err;
+                            console.log('successfully deleted profile img from directory');
+                          });
+    
+                        
+                    }
+                    
+                    
+    
+    
+                }
+                catch(err)
+                {
+                    console.log(err);
+    
+                    res.json({messsage: "no file uploaded"});
+                    
+                }
+                
+            }
+            else{
+                res.json({message:"Invalid email id"});
+                fs.unlink(path.join(path.dirname(process.mainModule.filename),'uploads',req.body.emailid+'.jpg')  , (err) => {
+                    if (err) throw err;
+                    console.log('successfully deleted profile img from directory');
+                  });
+            }
+            
+            
+        }
+    });
 
 })
 
