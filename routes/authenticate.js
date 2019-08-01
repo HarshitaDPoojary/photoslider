a2d551618cec8b111e93b2058b6cbe61df06390c
hp250497 <dmpoojary6458@gmail.com>
2019-08-01 12:50:38 +0530

    route for register

---
+++ routes/authenticate.js
@@ -0,0 +1,37 @@
+const express = require('express');
+var formidable=require('express-formidable');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const imageFilter=require('../filter');
+
+var multer = require('multer');
+var storage = multer.diskStorage(
+    {
+        destination: './uploads',
+        filename: function ( req, file, cb ) {
+            cb( null, req.body.emailid+'.jpg');
+        }
+    }
+);
+
+var upload = multer( { 
+    fileFilter:  function (req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+            return cb('Only jpeg/jpg/png images allowed', null);
+        }
+        cb(null, true);
+      },
+  storage: storage } );
+
+const userController = require('../controllers/user.controller');
+
+const router=express.Router();
+
+// router.use(bodyParser.json());
+// router.use(bodyParser.urlencoded({ extended: true }));
+
+router.post('/register',upload.single('file'),userController.create);
+
+
+
+module.exports = router;
